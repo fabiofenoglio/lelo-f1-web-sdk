c86f92a1e926b681979e7bb3203ce3220df617ef
Fabio Fenoglio <fabio.fenoglio@gmail.com>
2020-04-23 17:02:57 +0200

    added getKeyState function

--- example.js
+++ example.js
@@ -1,16 +1,33 @@
+function log(text) {
+    $('#log-container').append(text + '<br/>');
+}
+
 function runDemo() {
+    $("#demo-button").remove();
 
     const client = leloF1SdkClientProvider.getClient();
 
-    client.requestDevice().then(function(device) {
-        return client.connect(device);
+    log('looking for device ...');
+    client.searchAndConnect().then(function() {
+
+        log('device connected!');
+
+        return Promise.all([
+            client.getKeyState().then(function(keyState) {
+                log('key state: ' + keyState);
+            }),
+    
+            client.getBatteryLevel().then(function(batteryLevel) {
+                log('battery is at ' + batteryLevel + ' %');
+            })
+        ]);
     })
     .then(function() {
-        return client.getBatteryLevel().then(function(batteryLevel) {
-            alert('battery is at ' + batteryLevel + ' %');
-        });
-    })
-    .then(function() {
+        log('disconnecting');
+        client.disconnect();
+    }, function(err) {
+        log('disconnecting after error');
         client.disconnect();
     });
+
 }
