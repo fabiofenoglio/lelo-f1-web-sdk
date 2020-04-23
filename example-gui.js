f1db4411485d3df05cd152751cbc95fc9895f1aa
Fabio Fenoglio <development@fabiofenoglio.it>
2020-04-24 01:01:39 +0200

    gui layout

--- example-gui.js
+++ example-gui.js
@@ -5,7 +5,15 @@ app.controller('demoController', function($scope) {
     
     $scope.pending = 0;
     $scope.log = '';
+    $scope.errors = [];
     $scope.authorized = false;
+
+    // FAKE SIMULATION:
+    $scope.batteryLevel = 80;
+    $scope.temperature = 24.15;
+    $scope.pressure = 980.75;
+    $scope.acceleration = [100, 200, 1000];
+    $scope.depth = 80;
 
     $scope.isConnected = function() {
         return client && client.isConnected();
@@ -13,26 +21,29 @@ app.controller('demoController', function($scope) {
 
     $scope.connect = function() {
         $scope.pending ++;
+        refresh();
 
         log('connecting to device ...');
         client.searchAndConnect().then(function() {
             log('device connected!');
             $scope.pending --;
-            
+            $scope.authorized = false;
+
+            client.notifyKeyState(keyStateChanged);
+
             readDeviceInfo();
-            
             refresh();
 
         }, function(err) {
-            console.error('error connecting', err);
-            log('error connecting: ' + err);
+            error('error connecting', err);
             $scope.pending --;
-            refresh();
         });
     };
 
     $scope.disconnect = function() {
         $scope.pending ++;
+        refresh();
+
         log('disconnecting from device ...');
         client.disconnect().then(function() {
             log('device disconnected');
@@ -40,15 +51,15 @@ app.controller('demoController', function($scope) {
             refresh();
 
         }, function(err) {
-            console.error('error disconnecting', err);
-            log('error disconnecting: ' + err);
+            error('error disconnecting', err);
             $scope.pending --;
-            refresh();
         });
     };
 
     $scope.shutdown = function() {
         $scope.pending ++;
+        refresh();
+
         log('shutting down device ...');
         client.shutdown().then(function() {
             log('device shutdown and disconnected');
@@ -56,32 +67,45 @@ app.controller('demoController', function($scope) {
             refresh();
 
         }, function(err) {
-            console.error('error shutting down device', err);
-            log('error shutting down device: ' + err);
+            error('error shutting down device', err);
             $scope.pending --;
-            refresh();
         });
     };
 
     function readDeviceInfo() {
-        client.getBatteryLevel().then(function(value) {
-            $scope.batteryLevel = value;
-        });
-        client.getManufacturerName().then(function(value) {
-            $scope.manufacturerName = value;
-        });
-        client.getModelNumber().then(function(value) {
-            $scope.modelNumber = value;
-        });
-        client.getHardwareRevision().then(function(value) {
-            $scope.hardwareRevision = value;
-        });
-        client.getFirmwareRevision().then(function(value) {
-            $scope.firmwareRevision = value;
-        });
-        client.getSoftwareRevision().then(function(value) {
-            $scope.softwareRevision = value;
-        });
+        Promise.all([
+            client.getBatteryLevel().then(function(value) {
+                $scope.batteryLevel = value;
+            }),
+            client.getManufacturerName().then(function(value) {
+                $scope.manufacturerName = value;
+            }),
+            client.getModelNumber().then(function(value) {
+                $scope.modelNumber = value;
+            }),
+            client.getHardwareRevision().then(function(value) {
+                $scope.hardwareRevision = value;
+            }),
+            client.getFirmwareRevision().then(function(value) {
+                $scope.firmwareRevision = value;
+            }),
+            client.getSoftwareRevision().then(function(value) {
+                $scope.softwareRevision = value;
+            })
+        ]).then(refresh);
+    }
+
+    function keyStateChanged(value) {
+        $scope.authorized = value;
+        refresh();
+    }
+
+    function error(prefix, err) {
+        console.error(prefix, err)
+        log(prefix + ': ' + err);
+        $scope.errors.push(prefix + ': ' + err);
+        debugger;
+        refresh();
     }
 
     function log(text) {
